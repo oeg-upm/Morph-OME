227036a07c055b7455b965c0456d50642669c143
Ahmad Alobaid <ahmad88me@gmail.com>
2020-04-05 20:14:36 +0300

    generate rml mappings

--- util.py
+++ util.py
@@ -147,3 +147,77 @@ def generate_r2rml_mappings(mapping_file_dir, file_name, entity_class, entity_co
     f = open(mapping_file_dir, 'w')
     f.write(mapping_content.encode('utf8'))
     f.close()
+
+
+# def generate_r2rml_mappings(mapping_file_dir, file_name, entity_class, entity_column, mappings):
+# def generate_rml_mappings(file_name, entity_class, entity_column, mappings, file_url):
+def generate_rml_mappings_csv(mapping_file_dir, file_name, entity_class, entity_column, mappings):
+    mapping_id = get_random_string(10)
+    single_property_mapping = u"""
+        rr:predicateObjectMap [
+          rr:predicate %s;
+          rr:objectMap    [ rml:reference "%s" ]
+        ];
+    """
+    proper_mappings_list = [single_property_mapping % (m["val"].replace('http://schema.org/',''), m["key"]) for m in mappings]
+    property_column_mapping = "\n".join(proper_mappings_list)
+    mapping_file = u"""
+        @prefix rr: <http://www.w3.org/ns/r2rml#>.
+        @prefix rml: <http://semweb.mmlab.be/ns/rml#> .
+        @prefix ql: <http://semweb.mmlab.be/ns/ql#> .
+        @prefix mail: <http://example.com/mail#>.
+        @prefix xsd: <http://www.w3.org/2001/XMLSchema#>.
+        @prefix ex: <http://www.example.com/> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
+        @prefix transit: <http://vocab.org/transit/terms/> .
+        @prefix wgs84_pos: <http://www.w3.org/2003/01/geo/wgs84_pos#>.
+        @prefix schema: <http://schema.org/>.
+        @prefix gn: <http://www.geonames.org/ontology#>.
+        @prefix geosp: <http://www.telegraphis.net/ontology/geography/geography#> .
+        @base <http://mappingpedia.linkeddata.es/resource/> .
+        <%s>
+        rml:logicalSource [
+            rml:source  "%s";
+            rml:referenceFormulation ql:CSV
+
+        ];
+        rr:subjectMap [
+            rml:reference "%s";
+            rr:class schema:%s
+        ];
+        %s
+    .
+    """ % (mapping_id, file_name, entity_column, entity_class, property_column_mapping)
+    print mapping_file
+    mapping_file_path = mapping_file_dir
+    # mapping_file_path = os.path.join(BASE_DIR, 'local', mapping_id+'.rml.ttl')
+    print 'mapping file path:'
+    print mapping_file_path
+    f = open(mapping_file_path, 'w')
+    f.write(mapping_file.encode('utf8'))
+    f.close()
+    return mapping_file_path
+
+
+def get_json_path(j):
+    max_no = 0
+    json_path = []
+    for k in j.keys():
+        if isinstance(j[k], list):
+            # print "list (%d): " % len(j[k])
+            # print j[k]
+            if len(j[k]) > max_no:
+                # print "max list (%d): " % len(j[k])
+                # print j[k]
+                max_no = len(j[k])
+                json_path = [k]
+        elif isinstance(j[k], dict):
+            j_dict = get_json_path(j[k])
+            if j_dict["max_no"] > max_no:
+                max_no = j_dict["max_no"]
+                json_path = [k] + j_dict["json_path"]
+        # else:
+        #     print j[k]
+        #     print type(j[k])
+    return {"max_no": max_no, "json_path": json_path}
